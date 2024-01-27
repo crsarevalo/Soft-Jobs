1f45bad3a0a0e88d5cd98ef97a01bce528fc8f0f
Cristian Arevalo <crsarevalo@gmail.com>
2024-01-27 10:16:15 -0300

    fixing endpoints

--- backEnd/src/API/V1/routes/loginRoutes.js
+++ backEnd/src/API/V1/routes/loginRoutes.js
@@ -3,6 +3,6 @@ import { loginUser } from "../controllers/loginController.js";
 
 const router = express.Router();
 
-router.post("/auth_login", loginUser);
+router.post("/login", loginUser);
 
 export default router;
