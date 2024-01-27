f910e1dbc90bb0b24c73e1a912128b0d4ca4226b
Cristian Arevalo <crsarevalo@gmail.com>
2024-01-27 07:42:48 -0300

    fixing DB conection

--- backEnd/src/API/V1/controllers/usuariosController.js
+++ backEnd/src/API/V1/controllers/usuariosController.js
@@ -3,7 +3,7 @@ import { createUser } from "../models/usuariosModel.js";
 const createNewUser = async (req, res) => {
   try {
     const { user } = req.body;
-    console.log("Contenido del objeto user:", user);
+    //console.log("Contenido del objeto user:", user);
     const newUser = await createUser(user);
     res.status(201).json({ user: newUser });
   } catch (error) {
--- backEnd/src/API/V1/models/usuariosModel.js
+++ backEnd/src/API/V1/models/usuariosModel.js
@@ -1,15 +1,16 @@
 import pool from "../dataBase/usersDB.js";
 import bcrypt from "bcryptjs";
 
+console.log("Conectando a la base de datos:", process.env.DB_NAME);
+
 const createUser = async ({ email, password, rol, lenguaje }) => {
   const hashedPassword = bcrypt.hashSync(password);
-  console.log("Tipo de dato de la contraseña:", typeof hashedPassword);
 
   const SQLquery = {
     text: "INSERT INTO usuarios (email, password, rol, lenguage) VALUES ($1, $2, $3, $4) RETURNING *",
     values: [email, hashedPassword, rol, lenguaje],
   };
-  console.log("query:", SQLquery);
+  //console.log("query:", SQLquery);
 
   const response = await pool.query(SQLquery);
   return response.rows[0];
