56b624118b3db19fd9286a63d334d13320712592
Cristian Arevalo <crsarevalo@gmail.com>
2024-01-28 11:51:40 -0300

    done

--- backEnd/src/API/V1/routes/usuariosRoutes.js
+++ backEnd/src/API/V1/routes/usuariosRoutes.js
@@ -8,7 +8,6 @@ import { loginAuth } from "../../../../Middlewares/loginAuth.js";
 const router = express.Router();
 
 router.get("/usuarios", loginAuth, getAllUsers);
-// Ruta para registrar nuevos usuarios
 router.post("/usuarios", createNewUser);
 
 export default router;
